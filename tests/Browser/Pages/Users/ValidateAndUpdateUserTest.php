7e5aa39220e6287b8bb429ea78312b7cd2076e27
at-mailuong <mai.luong@asiantech.vn>
2018-06-01 16:06:31 +0700

    fix validate 2

--- tests/Browser/Pages/Users/ValidateAndUpdateUserTest.php
+++ tests/Browser/Pages/Users/ValidateAndUpdateUserTest.php
@@ -44,68 +44,11 @@ class ValidateAndUpdateUserTest extends DuskTestCase
     public function listCaseTestUpdateValidateForInput()
     {
         return [
+            ['full_name', '', 'TThe full name must be a string.'],
             ['address', '', 'The address must be a string.'],
-            ['phone', '', 'The phone format is invalid.'],
+            ['phone', '', 'The phone format is invalid'],
             ['identity_card', '', 'The identity card format is invalid.'],
         ];
-    }
-
-    /**
-     * Dusk test validate for input
-     *
-     * @param string $name name of field
-     * @param string $content content
-     * @param string $message message show when validate
-     * @param string $listUser list info user
-     * 
-     * @dataProvider listCaseTestUpdateValidateForInput
-     *
-     * @return void
-     */
-    public function testUpdateValidateForInput($name, $content, $message)
-    {
-        $this->browse(function (Browser $browser) use ($name, $content, $message) {
-            $browser->visit('admin/users/create')
-                ->press('Submit')                   
-                ->assertSee($message);
-        });
-    }
-
-    /**
-     * List case for test update user validate for input
-     *
-     * @return array
-     */
-    public function listCaseUpdateAlreadyTestValidateForInput()
-    {
-        return [
-            ['identity_card', '', 'The username has already been taken.'],
-        ];
-    }
-
-    /**
-     * Dusk test validate for input
-     *
-     * @param string $name name of field
-     * @param string $content content
-     * @param string $message message show when validate
-     * @param string $listUser list info user
-     * 
-     * @dataProvider listCaseAlreadyTestValidateForInput
-     *
-     * @return void
-     */
-    public function testUpdateValidateaAlreadyForInput($name, $content, $message)
-    {
-        factory('App\Models\UserInfo', 1)->create([
-            'idetity_card ' => '',
-        ]);       
-        $this->browse(function (Browser $browser) use ($name, $content, $message) {
-            $browser->visit('admin/users/create')
-                ->type('idetity_card', $content)
-                ->press('Submit')                   
-                ->assertSee($message);
-        });
     }
 
     /**
@@ -122,6 +65,7 @@ class ValidateAndUpdateUserTest extends DuskTestCase
                     ->type('full_name', 'mai luong')
                     ->type('address', 'quang nam')
                     ->type('phone', '0123345454')
+                    ->type('identity_card', '347368362')
                     ->press('Update')
                     ->assertPathIs('/admin/users')
                     ->assertSee('Update user successfully');                
@@ -129,6 +73,7 @@ class ValidateAndUpdateUserTest extends DuskTestCase
                 'full_name' => 'mai luong',
                 'address' => 'quang nam',
                 'phone' => '0123345454',
+                'identity_card' => '347368362',
             ]);
         });
     }
