2f938da1075d48112de1d1a60aaedc2273891948
at-khadinh <kha.dinh@asiantech.vn>
2018-07-18 11:20:41 +0700

    show cart info

--- public/js/public/cart.js
+++ public/js/public/cart.js
@@ -1,6 +1,50 @@
+var currency = '$';
+var cart;
+var totalCart = 0;
+
+function loadPage() {
+    if (!cart) {
+        cart = localStorage.getItem('PPMiniCart');
+        cart = JSON.parse(unescape(cart));
+    }
+
+    let products = cart.value.items;
+    let lengthCart = 0;
+    let productChild = $('.rem1').clone();
+
+    productChild.prop('hidden', false);
+    products.forEach(product => {
+        productChild.find('.invert-index').html(lengthCart + 1);
+        productChild.find('.invert-name').html(product.item_name);
+        productChild.find('.invert-price').html(currency + product.amount.toLocaleString());
+        productChild.find('.invert .value').attr('value', product.quantity);
+        productChild.find('.invert-image a').attr('href', product.href);
+        productChild.find('.invert-image .image-product-cart').attr('src', product.image_src);
+        $('.checkout-right .timetable_sub ').append(productChild.wrap('<tr class="rem1"><tr/>').parent().html());
+
+        $('<li>' + product.item_name + ' <i>-</i> <span>' + currency + (product.amount * product.quantity).toLocaleString() + ' </span></li>').insertBefore('.checkout-left .checkout-left-basket li:last-child');
+
+        totalCart += product.amount * product.quantity;
+        lengthCart++;
+    });
+
+    $('.checkout-left .checkout-left-basket li:last-child span').html(currency + totalCart.toLocaleString());
+    $('.checkout h2 span').html(lengthCart + ' ' + $('.checkout h2 span').html());
+}
+
+loadPage()
+
 $(document).ready(function() {
+
     $('.close1').on('click', function(){
         $(this).closest('.rem1').fadeOut('slow', function(){
+            let index = $(this).closest('.rem1').index();
+            totalCart = totalCart - (cart.value.items[index - 1].quantity * cart.value.items[index - 1].amount);
+            cart.value.items.splice(index - 1, 1);
+
+            localStorage.setItem('PPMiniCart', escape(JSON.stringify(cart)));
+            $('.checkout-left .checkout-left-basket li:nth-child(' + index + ')').remove();
+            $('.checkout-left .checkout-left-basket li:last-child span').html(currency + totalCart.toLocaleString());
             $(this).remove();
         });
     });
@@ -9,6 +53,7 @@ $(document).ready(function() {
         let divUpd = $(this).parent().find('.value'), newVal = parseInt(divUpd.val(), 10) + 1;
         if (newVal <= divUpd.attr('max') || !divUpd.attr('max')) {
             divUpd.val(newVal);
+            divUpd.change();
         }
     });
 
@@ -16,6 +61,32 @@ $(document).ready(function() {
         let divUpd = $(this).parent().find('.value'), newVal = parseInt(divUpd.val(), 10) - 1;
         if (newVal >= divUpd.attr('min')) {
             divUpd.val(newVal);
+            divUpd.change();
+        }
+    });
+
+    $('.value').on('change', function(){
+        let newVal = parseInt($(this).val());
+        let oldVal = parseInt($(this).attr('value'));
+        let index = $(this).closest('.rem1').index();
+        let newPrice = newVal * cart.value.items[index - 1].amount;
+        let oldPrice = oldVal * cart.value.items[index - 1].amount;
+        if (newVal > oldVal) {
+            $(this).attr('value', newVal);
+            totalCart = totalCart - oldPrice + newPrice;
+
+            cart.value.items[index - 1].quantity = newVal;
+            localStorage.setItem('PPMiniCart', escape(JSON.stringify(cart)));
+            $('.checkout-left .checkout-left-basket li:nth-child(' + index + ') span').html(currency + newPrice.toLocaleString());
+            $('.checkout-left .checkout-left-basket li:last-child span').html(currency + totalCart.toLocaleString());
+        } else if (newVal > 0) {
+            $(this).attr('value', newVal);
+            totalCart = totalCart - oldPrice + newPrice;
+
+            cart.value.items[index - 1].quantity = newVal;
+            localStorage.setItem('PPMiniCart', escape(JSON.stringify(cart)));
+            $('.checkout-left .checkout-left-basket li:nth-child(' + index + ') span').html(currency + newPrice.toLocaleString());
+            $('.checkout-left .checkout-left-basket li:last-child span').html(currency + totalCart.toLocaleString());
         }
     });
 });
--- resources/views/public/pages/cart.blade.php
+++ resources/views/public/pages/cart.blade.php
@@ -14,7 +14,7 @@
 <!-- checkout -->
   <div class="checkout">
     <div class="container">
-      <h2>{{ __('user/cart.my_cart_page.cart_contain_text') }} <span>3 Products</span></h2>
+      <h2>{{ __('user/cart.my_cart_page.cart_contain_text') }} <span>Products</span></h2>
       <div class="checkout-right">
         <table class="timetable_sub">
           <thead>
@@ -27,21 +27,21 @@
               <th>{{ __('user/cart.my_cart_page.remove') }}</th>
             </tr>
           </thead>
-          <tr class="rem1">
-            <td class="invert">1</td>
-            <td class="invert-image"><a href="single.html"><img class="image-product-cart" src="http://192.168.21.12/images/upload/img.jpg" alt=" " class="img-responsive" /></a></td>
+          <tr class="rem1" hidden>
+            <td class="invert invert-index"></td>
+            <td class="invert-image"><a href="#"><img class="image-product-cart" src="/images/upload/img.jpg" alt=" " class="img-responsive" /></a></td>
             <td class="invert">
                <div class="quantity">
                 <div class="quantity-select">
                   <div class="entry value-minus">&nbsp;</div>
-                  <input class="entry value no-spinners" type="number" value="0" min="0" max="10"/>
+                  <input class="entry value no-spinners" type="number" value="0" min="1" max=""/>
                   <div class="entry value-plus active">&nbsp;</div>
                 </div>
               </div>
             </td>
-            <td class="invert">Tata Salt</td>
+            <td class="invert invert-name">Tata Salt</td>
 
-            <td class="invert">$290.00</td>
+            <td class="invert invert-price">$290.00</td>
             <td class="invert">
               <div class="rem">
                 <div class="close1"> </div>
@@ -55,11 +55,7 @@
           <a href="#">{{ __('user/cart.my_cart_page.order_submit') }}</a>
           <a href="#">{{ __('user/cart.my_cart_page.add_coupon_code') }}</a>
           <ul>
-            <li>Product1 <i>-</i> <span>$15.00 </span></li>
-            <li>Product2 <i>-</i> <span>$25.00 </span></li>
-            <li>Product3 <i>-</i> <span>$29.00 </span></li>
-            <li>Total Service Charges <i>-</i> <span>$15.00</span></li>
-            <li>{{ __('user/cart.my_cart_page.total') }} <i>-</i> <span>$84.00</span></li>
+            <li>{{ __('user/cart.my_cart_page.total') }} <i>-</i> <span>$0.00</span></li>
           </ul>
         </div>
         <div class="checkout-right-basket">
